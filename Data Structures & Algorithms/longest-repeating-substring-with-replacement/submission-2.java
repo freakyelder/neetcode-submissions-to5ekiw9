1d00e7d8ac6a6848d3b74400c89282b213a9b1d0
freakyelder <165463239+freakyelder@users.noreply.github.com>
2026-03-31 23:53:03 +0530

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        int l=0;
+        int res=0;
+        int maxf=0;
+
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+
+            maxf=Math.max(maxf,map.get(ch));
+
+            while(((i-l+1)-maxf)>k){
+                char left=s.charAt(l);
+                map.put(left,map.get(left)-1);
+                l++;
+            }
+
+            res=Math.max(res,(i-l+1));
+        }
+
+        return res;
+    }
+}
