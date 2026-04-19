9ad5e031a700d717d8ff0c8b42cd02e72d720523
freakyelder <165463239+freakyelder@users.noreply.github.com>
2026-04-20 00:48:06 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 1) return 1;
+
+        int prev2 = 1; // f(n-2)
+        int prev1 = 1; // f(n-1)
+
+        for(int i = 2; i <= n; i++){
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+}
