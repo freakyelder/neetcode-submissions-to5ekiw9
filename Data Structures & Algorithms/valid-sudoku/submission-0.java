3606e5ef79e1878d3bd5a7f63176c7f541e82918
freakyelder <165463239+freakyelder@users.noreply.github.com>
2026-05-13 12:37:55 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> seen = new HashSet<>();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char ch = board[i][j];
+
+                if (ch == '.') continue;
+
+                String row = ch + " in row " + i;
+                String col = ch + " in col " + j;
+                String box = ch + " in box " + (i / 3) + "-" + (j / 3);
+
+                if (seen.contains(row) || seen.contains(col) || seen.contains(box)) {
+                    return false;
+                }
+
+                seen.add(row);
+                seen.add(col);
+                seen.add(box);
+            }
+        }
+
+        return true;
+    }
+}
